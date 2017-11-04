d437505cd30bab4040b36f40a01106f5ba1d0f8c
Jordan Liu <liu.jordan.com@gmail.com>
2017-11-04 01:11:36 -0400

    move intermediates to bottom

--- puzzle/models.py
+++ puzzle/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class user_level(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    level = models.ForeignKey(Level, on_delete=models.CASCADE)
-
-class user_stage(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
 
 class Level(models.Model):
     """ Model for a level of the game """
@@ -31,3 +24,13 @@ class Stage(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class user_level(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+
+
+class user_stage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
--- puzzle/views.py
+++ puzzle/views.py
@@ -6,3 +6,8 @@ def view_stage(request, stage_id):
     template = loader.get_template('stage_'+ stage_id +'.html')
     context = {}
     return HttpResponse(template.render(context, request))
+
+def view_home(request, user_id):
+    template = loader.get_template('home.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
