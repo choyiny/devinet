3a8ca5e88ea57e29c3e8c3b283dc78b22ccdfda4
Yong Cho Yin <choyiny@users.noreply.github.com>
2017-11-04 06:11:48 -0400

    dynamically loading pages without reload, and level 1

--- puzzle/views.py
+++ puzzle/views.py
@@ -1,23 +1,32 @@
+from django.shortcuts import render_to_response
 from django.template import loader
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 from .models import Level
 
 from puzzle.models import Stage
 
 
-def view_stage(request, pk):
+@csrf_exempt
+def view_stage(request):
     """
     Renders the template for a particular stage given a stage_id,
     if the user has the required permissions for it
     """
 
-    template = loader.get_template(Stage.objects.get(pk=pk).get_stage_url())
+    template = Stage.objects.get(pk=request.POST['pk']).get_stage_url()
+    context = {}
+    return render_to_response(template, context=context)
+
+
+def index(request):
+    template = loader.get_template('index.html')
     context = {}
     return HttpResponse(template.render(context, request))
 
-
 def view_home(request):
-    template = loader.get_template('home.html')
-    context = {'levels':Level.objects.all()}
+    template = 'home.html'
+    context = {'levels': Level.objects.all()}
 
-    return HttpResponse(template.render(context, request))
+    return render_to_response(template, context=context)
