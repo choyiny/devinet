4d6787bec9b540073a0ac994236f65d178b7e19a
Yong Cho Yin <choyiny@users.noreply.github.com>
2017-11-04 01:08:35 -0400

    fix formatting

--- puzzle/models.py
+++ puzzle/models.py
@@ -1,17 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class user_level(models.Model):
+
+class UserLevel(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     level = models.ForeignKey(Level, on_delete=models.CASCADE)
 
-class user_stage(models.Model):
+
+class UserStage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
+
 
 class Level(models.Model):
     """ Model for a level of the game """
     users = models.ManyToManyField(User, through='user_level')
+    
     def get_stages(self):
         """ Returns the stages for a specific level """
         return self.stage_set.objects.all()
