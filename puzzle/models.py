48455c60ef6f3718a33e392a424090b907019781
Yong Cho Yin <choyiny@users.noreply.github.com>
2017-11-04 00:11:36 -0400

    adding basic stage and level models

--- puzzle/models.py
+++ puzzle/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Stage(models.Model):
+    """ Model for a stage of a level """
+    # a stage has 1 level
+    level = models.ForeignKey(Level)
+
+    # name of stage
+    name = models.Textfield()
+
+    def __str__(self):
+        return self.name
+
+
+class Level(models.Model):
+    """ Model for a level of the game """
+
+    def get_stages(self):
+        """ Returns the stages for a specific level """
+        return self.stage_set.objects.all()
+
+    def __str__(self):
+        return "Level {}".format(self.id)
