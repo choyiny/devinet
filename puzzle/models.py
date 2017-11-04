a3c9592dd46bb4d5b917d7d2e2f82f9e645c82c2
Yong Cho Yin <choyiny@users.noreply.github.com>
2017-11-04 01:10:08 -0400

    Fix order

--- puzzle/models.py
+++ puzzle/models.py
@@ -2,20 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class UserLevel(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    level = models.ForeignKey(Level, on_delete=models.CASCADE)
-
-
-class UserStage(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
-
-
 class Level(models.Model):
     """ Model for a level of the game """
     users = models.ManyToManyField(User, through='user_level')
-    
+
     def get_stages(self):
         """ Returns the stages for a specific level """
         return self.stage_set.objects.all()
@@ -38,3 +28,13 @@ class Stage(models.Model):
         String representation for a stage. Intended for admin site.
         """
         return "{}: Stage {}".format(self.level, self.id)
+
+
+class UserLevel(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+
+
+class UserStage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
