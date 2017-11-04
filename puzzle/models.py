081698f80b4b96012d3f3a05c3085227728fdc52
Jordan Liu <liu.jordan.com@gmail.com>
2017-11-04 00:22:40 -0400

    added intermediates

--- puzzle/models.py
+++ puzzle/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class user_level(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    level = models.ForeignKey(Level, on_delete=models.CASCADE)
+
+class user_stage(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    stage = models.ForeignKey(Stage, on_delete=models.CASCADE)
