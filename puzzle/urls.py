519d260b248bb5115b2acfe4cfc65e44dc9175ec
Yong Cho Yin <choyiny@users.noreply.github.com>
2017-11-04 01:35:02 -0400

    stages view linked to template

--- puzzle/urls.py
+++ puzzle/urls.py
@@ -4,5 +4,5 @@ from puzzle import views
 
 app_name = 'profiles'
 urlpatterns = [
-    url(r'^(?P<stage_id>[0-9]+)/$', views.view_stage, name='view_stage'),
+    url(r'^(?P<pk>[0-9]+)/$', views.view_stage, name='view_stage'),
 ]
