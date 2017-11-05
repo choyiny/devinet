60ca6c0830c1a3e2c1e3c4d03e5401d176116f8f
Jordan Liu <liu.jordan.com@gmail.com>
2017-11-05 09:30:22 -0500

    fix url

--- devinet/urls.py
+++ devinet/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^puzzle/', include('puzzle.urls')),
+    url(r'^$', include('puzzle.urls')),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
 ]
