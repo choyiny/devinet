88f60bc9ef738970a21e9383bc4428b624a284f9
Jordan Liu <liu.jordan.com@gmail.com>
2017-11-05 04:26:21 -0500

    add user stage completeion to table after completing stagE

--- puzzle/urls.py
+++ puzzle/urls.py
@@ -6,6 +6,7 @@ app_name = 'puzzle'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^list/', views.view_list, name='list'),
+    url(r'^stage/stagecomplete/', views.setStageStatus, name='stage_complete'),
     url(r'^home/', views.view_home, name='home'),
     url(r'^stage/', views.view_stage, name='view_stage'),
 ]
