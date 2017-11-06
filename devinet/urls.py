a066b948696b5273a3581e9750f97781d42772f9
Yong Cho Yin <choyiny@users.noreply.github.com>
2017-11-06 15:13:59 -0500

    prevent crash

--- devinet/urls.py
+++ devinet/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^puzzle/', include('puzzle.urls')),
     url(r'^', include('puzzle.urls')),
     url(r'^oauth/', include('social_django.urls', namespace='social')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
