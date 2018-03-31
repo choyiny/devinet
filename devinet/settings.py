1c1a653e73ea5d4ccd15b52fb443a2c7b70ce2c2
Yong Cho Yin <choyiny@users.noreply.github.com>
2018-03-30 22:27:35 -0400

    fix CI error, set debug to true again

--- devinet/settings.py
+++ devinet/settings.py
@@ -26,7 +26,7 @@ if 'SECRET_KEY' in os.environ:
     SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     u'devinet.choy.in',
